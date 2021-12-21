aa25bba8472154b45de19e6941113b4fea98ae10
azertybae <90241183+azertybae@users.noreply.github.com>
2021-12-21 20:51:41 +0500

    Update z2.2.cpp

--- task 2/z2.2.cpp
+++ task 2/z2.2.cpp
@@ -1,1 +1,15 @@
+#include <iostream>
+using namespace std;
 
+int main()
+{
+    int n, proizv;
+    proizv = 1;
+    cin >> n;
+    for (int i = 1; i <= n; i++)
+    {
+        proizv = proizv * i;
+    }
+    cout << proizv;
+    return 0;
+}
