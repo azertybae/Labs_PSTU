7c7a7663e82519adaa3fd46ebd5bc7ac8190077c
azertybae <90241183+azertybae@users.noreply.github.com>
2021-12-27 07:47:28 +0500

    Update z2.6.cpp

--- task 2/z2.6.cpp
+++ task 2/z2.6.cpp
@@ -7,22 +7,22 @@ int main()
 	setlocale(LC_ALL, "RUS");
 	cin >> n;
 	if (n > 2) 
-  {
+  	{
 		for (int i = 0; i <= n; i++) 
-    {
+    		{
 			for (int j = 0; j < i; j++) 
-      {
+      			{
 				cout << " ";
 			}
 			for (int j = i; j <= n-1; j++) 
-      {
+      			{
 				cout << "*";
 			}
 			cout << endl;
 		}
 	}
 	else 
-  {
+  	{
 		cout << "Введённое число меньше 2";
 	}
 	return 0;
