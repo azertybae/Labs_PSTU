9cc0b34c9ad7e1ca677704bd44d2cb5c40911aeb
azertybae <90241183+azertybae@users.noreply.github.com>
2021-12-27 14:00:15 +0500

    Create newton.cpp

---
+++ uravn/newton.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main() 
+{
+    setlocale(LC_ALL, "Russian");
+    float x, xPrev;
+    x = 2;
+    xPrev = 0;
+    double eps = 0.000001;
+    while (abs(x - xPrev) > eps) 
+    {
+        xPrev = x;
+        x = xPrev - ((pow(xPrev, 3) - xPrev - 1) / (3 * pow(xPrev, 2) - 1));
+    }
+    cout << "Корень: " << x << endl;
+    return 0;
+}
